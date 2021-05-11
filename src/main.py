5a9c5be32fea3b230e57f3fe0f05ee264df9880c
Sathvik PN <sathvikapn@gmail.com>
2021-05-11 23:35:48 +0530

    add encrypt_decrypt functionality to process raw data with appropriate password

--- src/main.py
+++ src/main.py
@@ -2,3 +2,12 @@ from hashlib import md5
 from base64 import urlsafe_b64encode
 from cryptography.fernet import Fernet
 
+def encrypt_decrypt(string, password, mode='encode'):
+    """ Encodes or Decodes raw data w.r.t password based on mode specified. """
+    _hash = md5(password.encode()).hexdigest() # get hash of password
+    cipher_key = urlsafe_b64encode(_hash.encode()) # use the hash as the key of encryption
+    cipher = Fernet(cipher_key) # get the cipher based on the cipher key
+    if mode == 'encode':
+        return cipher.encrypt(string.encode()).decode() #encrypt the data
+    else:
+        return cipher.decrypt(string.encode()).decode() #decrypt the data
