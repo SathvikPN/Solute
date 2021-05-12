0e9584ea702afa8ddd95971f9dc145f48706b1bf
Sathvik PN <sathvikapn@gmail.com>
2021-05-12 15:33:05 +0530

    Created GUI file for application and verified local imports

---
+++ app.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Graphical User Interface for Steganography Application
+Author: Sathvik PN
+GitHub: https://github.com/SathvikPN
+"""
+
+from src import core, utility, custom_exceptions 
