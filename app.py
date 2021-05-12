161b19ecaa30cd2cb89249526b53fb8589984401
Sathvik PN <sathvikapn@gmail.com>
2021-05-12 15:39:41 +0530

    add display window method in UI MainWindow for info

--- app.py
+++ app.py
@@ -6,3 +6,34 @@ GitHub: https://github.com/SathvikPN
 """
 
 from src import core, utility, custom_exceptions 
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+# -----------------------------------------------
+class UI_MainWindow():
+
+    # Function to display Message/Error/Information 
+    def display_msg(self, title, msg, ico_type=None):
+        MsgBox = QtWidgets.QMessageBox()
+        MsgBox.setText(msg)
+        MsgBox.setWindowTitle(title)
+
+        if ico_type == 'err':
+            ico = QtWidgets.QMessageBox.critical
+        else:
+            ico = QtWidgets.QMessageBox.information
+
+        MsgBox.setIcon(ico)
+        MsgBox.exec()
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = UI_MainWindow()
+    MainWindow.show()
+    sys.exit(app.exec_())
