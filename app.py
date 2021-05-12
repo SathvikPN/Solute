3786f7dc30b6d56803211ca54e920c89ab843b7f
Sathvik PN <sathvikapn@gmail.com>
2021-05-12 17:33:22 +0530

    fix typo

--- app.py
+++ app.py
@@ -338,7 +338,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = UI_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
