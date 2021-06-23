6632eae0716aba25eff112c4351f38518fda69db
Sathvik PN <sathvikapn@gmail.com>
2021-06-23 16:13:05 +0530

    add Header and tagline

--- solute_ui.py
+++ solute_ui.py
@@ -23,13 +23,40 @@ class SoluteApp(tk.Tk):
 
         self.title("Solute")
         self.protocol("WM_DELETE_WINDOW", self.exit)
-    
+
+        # Layout Management
+        total_rows = 7
+        total_columns = 4
+
+        for r in range(total_rows):
+            self.grid_rowconfigure(r, pad=10)
+
+        for c in range(total_columns):
+            self.grid_columnconfigure(c, pad=10)
+
+        self.grid_columnconfigure((1,2), weight = 4)
+
+
+        # Header Section
+        HEADER = "SOLUTE"
+        header = tk.Label(self)
+        header.config(text=HEADER)
+        header.config(font=('Verdana',20))
+        header.grid(row=0, column=0, columnspan=3)
+
+        # Tagline section
+        TAG_LINE = "Simplified steganography tool for your data privacy"
+        tagline = tk.Label(self)
+        tagline.config(text=TAG_LINE)
+        tagline.config(font=('Verdana',12))
+        tagline.grid(row=1, column=0, columnspan=3, sticky='n')
+
 
     def exit(self):
         # Exit Confirmation Message box
         TITLE = "Exit"
         MESSAGE = "Are you sure want to exit?"
-        
+
         sure = messagebox.askyesno(TITLE, MESSAGE )
         if sure is True:
             self.destroy()
