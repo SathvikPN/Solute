f5b907027c2b80452cc7b5bad28c4edf55a08d76
Sathvik PN <sathvikapn@gmail.com>
2021-06-23 17:54:36 +0530

    code refactor. Only static section placed in Root window

--- solute_ui.py
+++ solute_ui.py
@@ -7,7 +7,8 @@ Project link: https://github.com/SathvikPN/Solute
 
 # Imports -----------------------------------------------------
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import BitmapImage, messagebox
+from tkinter import filedialog
 
 
 # Root Window of Application
@@ -24,32 +25,29 @@ class SoluteApp(tk.Tk):
         self.title("Solute")
         self.protocol("WM_DELETE_WINDOW", self.exit)
 
-        # Layout Management
-        total_rows = 7
-        total_columns = 4
-
-        for r in range(total_rows):
-            self.grid_rowconfigure(r, pad=10)
-
-        for c in range(total_columns):
-            self.grid_columnconfigure(c, pad=10)
-
-        self.grid_columnconfigure((1,2), weight = 4)
-
-
         # Header Section
         HEADER = "SOLUTE"
         header = tk.Label(self)
         header.config(text=HEADER)
         header.config(font=('Verdana',20))
-        header.grid(row=0, column=0, columnspan=3)
+        header.pack(side='top')
 
         # Tagline section
         TAG_LINE = "Simplified steganography tool for your data privacy"
         tagline = tk.Label(self)
         tagline.config(text=TAG_LINE)
         tagline.config(font=('Verdana',12))
-        tagline.grid(row=1, column=0, columnspan=3, sticky='n')
+        tagline.pack()
+
+        # Load Image
+        # self.image = filedialog.askopenfilename(filetypes=[("Image files",".png .jpeg .jpg .bmp")])
+
+
+        # Footer section
+        FOOTER = "Developed by [ SathvikPN ]"
+        footer = tk.Label(self)
+        footer.config(text=FOOTER)
+        footer.pack(side='bottom')
 
 
     def exit(self):
