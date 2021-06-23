2f4c0a9f3815bee717830efba1d24ec4144f0314
Sathvik PN <sathvikapn@gmail.com>
2021-06-23 19:04:41 +0530

    add workspace page. Replaced pack with grid management

--- solute_ui.py
+++ solute_ui.py
@@ -7,7 +7,7 @@ Project link: https://github.com/SathvikPN/Solute
 
 # Imports -----------------------------------------------------
 import tkinter as tk
-from tkinter import BitmapImage, messagebox
+from tkinter import BitmapImage, Frame, messagebox
 from tkinter import filedialog
 
 
@@ -25,29 +25,65 @@ class SoluteApp(tk.Tk):
         self.title("Solute")
         self.protocol("WM_DELETE_WINDOW", self.exit)
 
+        # Layout Configuration 
+        self.grid_rowconfigure((0,1,9), weight=1)
+        self.grid_rowconfigure((2,3,4,5,6,7,8), weight=2)
+        self.grid_columnconfigure((0,1), weight=1)
+
         # Header Section
         HEADER = "SOLUTE"
         header = tk.Label(self)
         header.config(text=HEADER)
         header.config(font=('Verdana',20))
-        header.pack(side='top')
+        header.grid(row=0, column=0, columnspan=2)
 
         # Tagline section
         TAG_LINE = "Simplified steganography tool for your data privacy"
         tagline = tk.Label(self)
         tagline.config(text=TAG_LINE)
         tagline.config(font=('Verdana',12))
-        tagline.pack()
+        tagline.grid(row=1, column=0, columnspan=2)
+        tagline.grid_anchor('s')
 
         # Load Image
         # self.image = filedialog.askopenfilename(filetypes=[("Image files",".png .jpeg .jpg .bmp")])
 
 
+        # Configure container inside root window 
+        # Container frame serves pages
+        container = tk.Frame(self)
+        container.grid(row=2, column=0)
+
         # Footer section
         FOOTER = "Developed by [ SathvikPN ]"
         footer = tk.Label(self)
         footer.config(text=FOOTER)
-        footer.pack(side='bottom')
+        footer.grid(row=9, column=0, columnspan=2, pady=5, sticky='s')
+        
+
+
+
+        self.frames = {}
+        self.pages = [Workspace]
+        for F in self.pages:    
+            # THIS_CASE: F = Workspace
+            page_name = F.__name__ 
+            # IF_CASE: F=Workspace() ==> page_name=F.__class__.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+        # Display Starting page in container
+        self.show_frame("Workspace")
+
+
+    def show_frame(self, page_name):
+        """ Display passed page 
+        
+        Multiple pages are stacked at same container
+        raises passed page to top of stack which is displayed
+        """
+        frame = self.frames[page_name]
+        frame.tkraise()
 
 
     def exit(self):
@@ -63,8 +99,17 @@ class SoluteApp(tk.Tk):
     def run(self):
         self.mainloop()
 
+class Workspace(tk.Frame):
+    """ Dynamic content of the app served here """
+    def __init__(self, parent, controller):
+        super().__init__(parent)
 
+        self.controller = controller
 
+        btn_start = tk.Label(text="Center of Workspace")
+        btn_start.grid(row=2, column=0, rowspan=7, columnspan=2)
+
+        
 if __name__=='__main__':
     app = SoluteApp()
     app.run()
