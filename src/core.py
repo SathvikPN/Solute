4120770097f0c05ac95d0a58fc86b7728f048183
Sathvik PN <sathvikapn@gmail.com>
2021-06-24 11:21:42 +0530

    initialise core module of application

---
+++ src/core.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" Solute App
+LSB Steganography Application
+- Author: Sathvik PN
+- GitHub: https://github.com/SathvikPN/Steganography-application
+"""
+
+from hashlib import sha256
