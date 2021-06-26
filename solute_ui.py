8ef69f08d8799eb10a733f248a977ebd1eb94e6a
Sathvik PN <sathvikapn@gmail.com>
2021-06-26 23:48:53 +0530

    connected core application module to GUI

--- solute_ui.py
+++ solute_ui.py
@@ -6,13 +6,19 @@ Project link: https://github.com/SathvikPN/Solute
 
 
 # Imports -----------------------------------------------------
+
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import filedialog
 from tkinter import ttk
 from tkinter import font
-from tkinter.constants import VERTICAL
+
+from src import core
 # -------------------------------------------------------------
+
+# Options 
+TEXT_BOX_HEIGHT = 8
+PADDINGS = {'padx':10, 'pady':10}
 
 class SoluteApp(tk.Tk):
     """ Root Window of application """
@@ -143,8 +149,8 @@ class Workspace(tk.Frame):
         step2_enc_info = tk.Label(text="Enter text to hide")
         step2_enc_info.grid(row=5, column=0, sticky='w', **PADDINGS)
 
-        encode_data = tk.Text(height=TEXT_BOX_HEIGHT)
-        encode_data.grid(row=6,column=0, rowspan=4, columnspan=3, **PADDINGS) #   
+        self.encode_data = tk.Text(height=TEXT_BOX_HEIGHT)
+        self.encode_data.grid(row=6,column=0, rowspan=4, columnspan=3, **PADDINGS) #   
 
         step3 = tk.Label(text="Step 3")
         step3.grid(row=10, column=0, sticky='w', **PADDINGS)
@@ -153,14 +159,15 @@ class Workspace(tk.Frame):
     
         step3_info.grid(row=11, column=0, sticky='w', **PADDINGS)
 
-        pwd_enc = tk.Entry()
-        pwd_enc.config(show='*')
-        pwd_enc.grid(row=11, column=1, columnspan=2, sticky='w', **PADDINGS)
+        self.pwd_enc = tk.Entry()
+        self.pwd_enc.config(show='*')
+        self.pwd_enc.grid(row=11, column=1, columnspan=2, sticky='w', **PADDINGS)
 
         # ROW:12 COLUMN:(0,1,2) --> ADD PROGRESS BAR
 
         encode_btn = tk.Button(text="Encode and Save")
         encode_btn.config(font=8)
+        encode_btn.config(command=self.save_encode)
         encode_btn.grid(row=13, rowspan=2, column=0, columnspan=3, sticky='ns')
         
 
@@ -176,12 +183,13 @@ class Workspace(tk.Frame):
         step2_dec_info = tk.Label(text="Enter Password")
         step2_dec_info.grid(row=5, column=3, sticky='w', **PADDINGS)
 
-        pwd_dec = tk.Entry()
-        pwd_dec.config(show='*')
-        pwd_dec.grid(row=5, column=4, columnspan=2, sticky='ew', **PADDINGS)
+        self.pwd_dec = tk.Entry()
+        self.pwd_dec.config(show='*')
+        self.pwd_dec.grid(row=5, column=4, columnspan=2, sticky='ew', **PADDINGS)
 
         decode_btn = tk.Button(text="Decode")
         decode_btn.config(font=8)
+        decode_btn.config(command=self.decode)
         decode_btn.grid(row=6, column=4, sticky='ns')
 
         # ROW:7 COLUMNS:(3,4,5) ---> ADD PROGRESS BAR
@@ -189,26 +197,66 @@ class Workspace(tk.Frame):
         dec_info = tk.Label(text="Decoded data")
         dec_info.grid(row=8, column=3, sticky='w', **PADDINGS)
 
-        dec_data = tk.Text(height=TEXT_BOX_HEIGHT)
-        dec_data.insert('1.0', DECODED_DATA)
-        dec_data.grid(row=10, column=3, rowspan=4,columnspan=3,sticky='w', **PADDINGS ) # 
+        self.dec_data = tk.Text(height=TEXT_BOX_HEIGHT)
+        self.dec_data.grid(row=10, column=3, rowspan=4,columnspan=3,sticky='w', **PADDINGS ) # 
 
 
 
 
 
     def image_selector(self):
+        self.step1_path.delete('0', 'end')
         FILE_TYPES = ([('Image Files',('.png','.jpg','.jpeg'))])
-        img_path = filedialog.askopenfilename(filetypes = FILE_TYPES)
-        if not img_path:
+        self.img_path = filedialog.askopenfilename(filetypes = FILE_TYPES)
+        if not self.img_path:
             messagebox.showerror("No Image found", "Please select an image")
         else:
-            self.step1_path.insert('0',img_path)
-    
+            self.step1_path.insert('0',self.img_path)
+            return self.step1_path
 
-
-
+    def save_encode(self):
+        # self.step1_path = tk.Entry()
+        # self.encode_data = tk.Text(height=TEXT_BOX_HEIGHT)
+        # self.pwd_enc = tk.Entry()
+        self.enc_img = filedialog.asksaveasfilename(title='Save file') # filetypes=[("PNG", ".png")]
+        self.enc_img = self.enc_img+'.png'
+        try:
+            core.encode_img(
+                self.step1_path.get(),
+                self.encode_data.get("1.0","end-1c"),
+                self.enc_img,
+                self.pwd_enc.get()
+            )
         
+        except core.DataOverflowError:
+            messagebox.showwarning(
+                title="Data Overflow",
+                message="Data size is too big to fit inside this image"
+            )
+        messagebox.showinfo(
+            title="Encoding Successful",
+            message="Successfully created the Encoded Image.\nYay!"
+        )
+    
+    def decode(self):
+        # self.pwd_dec = tk.Entry()
+        try:
+            self.dec_data.insert(
+                "1.0", 
+                core.decode_img(self.step1_path.get(), self.pwd_dec.get())
+            )
+            messagebox.showinfo(
+                title="Decode Success",
+                message="Successfully extracted all encoded data from image.\nYay!"
+            )
+
+        except core.PasswordError:
+            messagebox.showerror(
+                title="Incorrect Password",
+                message="Please ensure correct image and correct password is entered."
+            )
+            self.pwd_dec.delete('0', 'end')
+
 
 
 
@@ -216,12 +264,5 @@ class Workspace(tk.Frame):
 
 # APP TRIGGER -------------------------------------------------
 if __name__=='__main__':
-    DEBUG = True
-    if DEBUG is True:
-        DECODED_DATA = "DEBUG Mode Active."
-        TEXT_BOX_HEIGHT = 8
-        PADDINGS = {'padx':10, 'pady':10}
-
-
     app = SoluteApp()
     app.run()
