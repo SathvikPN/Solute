fc5edf4354a3b7a6d411c0e09e70f6e5403051af
Sathvik PN <sathvik-pn@hpe.com>
2022-06-25 18:25:53 +0530

    add ReadImageError: custom exception

--- solute/core.py
+++ solute/core.py
@@ -5,9 +5,11 @@ from PIL import Image
 import numpy as np
 
 from exceptions import InvalidModeError
+from utility import string_to_binary, binary_to_string
+
 
 # ENCRYPT-DECRYPT Text Data ---------------------------------------------------
-def encrypt_decrypt(data_string:str, password:str, mode='encrypt'):
+def encrypt_decrypt(data_string:str, password:str, mode='encrypt') -> str:
     """Encrypts OR Decrypts data_string w.r.t password based on mode specified 
     Parameters:
         data_string: Text data for cryptic transformation.
@@ -43,3 +45,35 @@ def encrypt_decrypt(data_string:str, password:str, mode='encrypt'):
     else:
         error_msg = "Invalid mode for encrypt_decrypt() \nexpected {'encrypt', 'decrypt'}"
         raise InvalidModeError(error_msg)
+
+
+# ENCODER section -------------------------------------------------------------
+def encode_img(input_img:str, text:str, output_img:str, password:str='') -> None:
+    """ Create an Image encoded with text data.
+
+    Parameters:
+        input_img : path to input image file
+        text : text data that needs to be encoded
+        output_img : path to write output image 
+        password : key to encrypt text data
+    
+    Returns:
+        None
+    """    
+    if password:
+        data = encrypt_decrypt(text, password, mode='encrypt')
+    else:
+        data = text 
+
+    # process data: 32-bit info about length of data to be encoded
+    data_length = bin(len(data))[2:]    # eg: '0b11101' --> '11101'
+    data_length = data_length.zfill(32)
+
+    # encode data prefixed with data-length
+    bin_data = iter(data_length + string_to_binary(data))
+
+    # read cover image 
+    try:
+        img = Image.open(input_img)
+    except:
+        # error
--- solute/exceptions.py
+++ solute/exceptions.py
@@ -2,3 +2,6 @@
 
 class InvalidModeError(Exception):
     pass
+
+class ReadImageError(Exception):
+    pass
